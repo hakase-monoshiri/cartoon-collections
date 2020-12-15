3a2a0e24b4347a7cb614477f947afee7e6288496
hakase-monoshiri <ebenaeleazer@outlook.com>
2020-12-15 18:44:04 -0500

    commit tree 5d71a9bf8bfa6a70212be271949e102c734a4990

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves_array)
+  dwarves_array.each do |name| 
+    puts " #{dwarves_array.index(name) + 1} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |call|
+    call.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.detect do |snack|
+    cheese_types.include?(snack) 
+  end
 end
